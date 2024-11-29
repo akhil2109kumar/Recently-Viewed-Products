4d596fbb4062000affd5c408c6fdae70a40fa721
dilip175 <dilip@drivelah.sg>
2024-11-29 15:27:14 +0530

    Updated cors code

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const setupSwagger = require("./utils/swagger");
 
 const app = express();
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use("/api/v1/users", userRoutes);
 setupSwagger(app);
